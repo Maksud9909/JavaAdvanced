10392a4c1479febb7cb16d7cae4e2ba24ef01197
maksud9909 <rmaksud822@gmail.com>
2023-05-09 20:42:24 +0500

    BinarySearch

--- src/Collection/BinarySearch/BinarySearch.java
+++ src/Collection/BinarySearch/BinarySearch.java
@@ -1,9 +1,6 @@
 package Collection.BinarySearch;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class BinarySearch {
     public static void main(String[] args) {
@@ -24,22 +21,39 @@ public class BinarySearch {
 //        System.out.println(index1);
 
 
-        Employee emp1 = new Employee(100,"Maksud",1224);
-        Employee emp2 = new Employee(100,"Anton",3094);
-        Employee emp3 = new Employee(100,"Andrei",84603);
-        Employee emp4 = new Employee(100,"Anderson",356);
-        Employee emp5 = new Employee(100,"Gas",4567);
-        Employee emp6 = new Employee(100,"Zaur",567);
-        Employee emp7 = new Employee(100,"Rahmon",345);
-        List<Employee> employeeList = new ArrayList<>();
-        employeeList.add(emp1);
-        employeeList.add(emp2);
-        employeeList.add(emp3);
-        employeeList.add(emp4);
-        employeeList.add(emp5);
-        employeeList.add(emp6);
-        employeeList.add(emp7);
-        System.out.println(employeeList);
+//        Employee emp1 = new Employee(100,"Maksud",1224);
+//        Employee emp2 = new Employee(35,"Anton",3094);
+//        Employee emp3 = new Employee(345,"Andrei",84603);
+//        Employee emp4 = new Employee(849,"Anderson",356);
+//        Employee emp5 = new Employee(34,"Gas",4567);
+//        Employee emp6 = new Employee(56,"Zaur",567);
+//        Employee emp7 = new Employee(100,"Rahmon",345);
+//        List<Employee> employeeList = new ArrayList<>();
+//        employeeList.add(emp1);
+//        employeeList.add(emp2);
+//        employeeList.add(emp3);
+//        employeeList.add(emp4);
+//        employeeList.add(emp5);
+//        employeeList.add(emp6);
+//        employeeList.add(emp7);
+//        System.out.println(employeeList);
+//        Collections.sort(employeeList);
+//        System.out.println(employeeList);
+//
+//
+//
+//        int index2 = Collections.binarySearch(employeeList,new Employee(849,"Anderson",356));
+//        System.out.println(index2);
+
+
+        int [] array = {-3, 8, 12, -8, 0, 5, 10, 1, 150, -30, 19};
+        System.out.println(Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+        int index3 = Arrays.binarySearch(array,150);
+        System.out.println(index3);
+
+        System.out.println(array);
     }
 }
 
@@ -63,7 +77,11 @@ class Employee implements Comparable<Employee> {
     }
 
     @Override
-    public int compareTo(Employee o) {
-        return 0;
+    public int compareTo(Employee anotherEmp) {
+        int resultId =  this.id - anotherEmp.id;
+        if (resultId == 0){
+             resultId =  this.name.compareTo(anotherEmp.name);
+        }
+        return resultId;
     }
 }
