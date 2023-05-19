ca3dd270f878fc57209d91a802e9e026978d55cf
maksud9909 <rmaksud822@gmail.com>
2023-05-19 22:18:55 +0500

    All other stuff

---
+++ src/Delete.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Delete {
+    public static void main(String[] args) {
+        Integer[] arr = {1, 1,3,3,3,3};
+
+        List<Integer> list = Arrays.asList(arr);
+        System.out.println(list);
+
+
+
+
+    }
+
+}
