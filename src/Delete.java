e00a0a8528c99e098c216307c83b7e4eaba3bca1
maksud9909 <rmaksud822@gmail.com>
2023-05-24 18:26:06 +0500

    update 2

--- src/Delete.java
+++ src/Delete.java
@@ -1,12 +1,39 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import javax.management.DescriptorKey;
+import java.util.*;
 
 public class Delete {
+    String name;
+    int salary;
     public static void main(String[] args) {
-        int arr [] = {1,2,3,4,4,5,6};
-        System.out.println();
 
+        Delete delete1 = new Delete("Maksud",100);
+        Delete delete2 = new Delete("Maksud",100);
+        System.out.println(delete1.equals(delete2));
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Delete delete = (Delete) o;
+        return Objects.equals(name, delete.name);
+    }
+
+    public Delete(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Delete{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name);
+//    }
 }
