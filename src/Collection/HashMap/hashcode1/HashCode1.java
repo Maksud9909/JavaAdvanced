39f5440ab0b937ff4196ec1ffcb83ea73588c937
maksud9909 <rmaksud822@gmail.com>
2023-06-16 21:08:08 +0500

    Student and hashcode

--- src/Collection/HashMap/hashcode1/HashCode1.java
+++ src/Collection/HashMap/hashcode1/HashCode1.java
@@ -31,38 +31,3 @@ public class HashCode1 {
 
 }
 
-final class Student {
-     final String name;
-    final String surname;
-     final int course;
-
-    public Student(String name, String surname, int course) {
-        this.name = name;
-        this.surname = surname;
-        this.course = course;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", course=" + course +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return course == student.course && Objects.equals(name, student.name) && Objects.equals(surname, student.surname);
-    }
-
-    @Override
-    public int hashCode() {
-        return name.length() * 7 + surname.length() * 134 + course * 53;
-    }
-    // hashcode - это когда результаты превращаются в int, чтобы они не были одинаковыми
-    // затем эти элементы умножаются и получаются разные значения, чем больше цифры, тем меньше будет совпадений
-}
