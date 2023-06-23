6989ddb85e200c5a5adda012291d4a5e3cb078a1
maksud9909 <rmaksud822@gmail.com>
2023-06-23 18:01:45 +0500

    Others thing

---
+++ src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("black pink");
+        System.out.println("black pink so cute and so happy");
+    }
+}
---
+++ src/Z/Test2.java
@@ -0,0 +1,16 @@
+package Z;
+
+import java.util.LinkedHashMap;
+
+public class Test2 {
+    public static void main(String[] args) {
+        LinkedHashMap<String,Integer> hashMap = new LinkedHashMap<>(16,0.75f,true);
+        hashMap.put("Lavash",7);
+        hashMap.put("Ocean food",4);
+        hashMap.put("Rolls",4);
+        hashMap.put("Sushi",5);
+        System.out.println(hashMap.get("Rolls"));
+        System.out.println(hashMap);
+
+    }
+}
