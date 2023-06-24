8707c78f99069c8eea88838d80cfefdd60010718
maksud9909 <rmaksud822@gmail.com>
2023-06-24 16:41:35 +0500

    LinkedHashSetEx1.java

---
+++ src/Collection/LinkedHashSet/LinkedHashSetEx1.java
@@ -0,0 +1,21 @@
+package Collection.LinkedHashSet;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetEx1 {
+    public static void main(String[] args) {
+        LinkedHashSet<Integer> hashSet = new LinkedHashSet<>(16,0.75f);
+        hashSet.add(18);
+        hashSet.add(19);
+        hashSet.add(76);
+        hashSet.add(1349);
+        System.out.println(hashSet);
+        System.out.println(hashSet.remove(19));
+        System.out.println(hashSet);
+
+
+        System.out.println(hashSet.contains(1349));
+
+
+    }
+}
