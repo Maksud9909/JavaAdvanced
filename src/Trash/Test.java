0ac5960a8afe81d65f9184400658196689e6b1b3
maksud9909 <rmaksud822@gmail.com>
2023-07-19 18:25:22 +0500

    All unsaved files

---
+++ src/Trash/Test.java
@@ -0,0 +1,15 @@
+package Trash;
+
+public class Test {
+
+    public static class Point {
+        int x;
+        int y;
+
+    }
+
+    public static void main(String[] args) {
+        Point point = new Point();
+        System.out.println(point);
+    }
+}
