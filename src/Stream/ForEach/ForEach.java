e7b41c8dd8839a05e55bc8a992be0f17d4fc6c21
maksud9909 <rmaksud822@gmail.com>
2023-07-28 14:22:42 +0500

    Filter.java

--- src/Stream/ForEach/ForEach.java
+++ src/Stream/ForEach/ForEach.java
@@ -1,5 +1,14 @@
 package Stream.ForEach;
 
-public class ForEach {
+import java.util.Arrays;
 
+public class ForEach {
+    public static void main(String[] args) {
+        int arr [] = {4,6,3,8,2};
+        Arrays.stream(arr).forEach(numbers -> {
+            numbers *= 2;
+            System.out.print(numbers + " ");
+        });
+
+    }
 }
