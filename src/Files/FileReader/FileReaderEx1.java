ff24102d3cdba0e9be494976b3f8829aaa0bba69
maksud9909 <rmaksud822@gmail.com>
2023-08-30 10:20:45 +0500

    All files

--- src/Files/FileReader/FileReaderEx1.java
+++ src/Files/FileReader/FileReaderEx1.java
@@ -1,25 +1,45 @@
 package Files.FileReader;
 
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class FileReaderEx1 {
     public static void main(String[] args) throws IOException {
-        FileReader reader = null;
+        FileReader fileReader = null;
         try {
-             reader = new FileReader("test2.txt");
-             int character;
-             while ((character=reader.read())!=-1) {
-                 System.out.print((char) character);
-             }
-            System.out.println();
-            System.out.println("Done");
-        } catch (IOException e) {
+            fileReader = new FileReader("test2.txt");
+            int character;
+            while ((character = fileReader.read())!=-1){
+                System.out.print((char) character);
+            }
+
+
+        } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
-        }
-        finally {
-            reader.close();
         }
     }
 }
+
+
+
+
+////public static void main(String[] args) throws IOException {
+//    FileReader reader = null;
+//    try {
+//         reader = new FileReader("test2.txt");
+//         int character;
+//         while ((character=reader.read())!=-1) {
+//             System.out.print((char) character);
+//         }
+//        System.out.println();
+//        System.out.println("Done");
+//    } catch (IOException e) {
+//        throw new RuntimeException(e);
+//    }
+//    finally {
+//        reader.close();
+//    }
+//}
