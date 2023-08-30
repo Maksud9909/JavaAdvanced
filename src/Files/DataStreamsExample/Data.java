f2d3a979ce8f78fe65c648a897054de33396db47
maksud9909 <rmaksud822@gmail.com>
2023-08-30 11:46:34 +0500

    DataInputStream

--- src/Files/DataStreamsExample/Data.java
+++ src/Files/DataStreamsExample/Data.java
@@ -1,4 +1,32 @@
 package Files.DataStreamsExample;
 
+import java.io.*;
+
 public class Data {
+    public static void main(String[] args) {
+        try (
+                DataOutputStream outputStream = new DataOutputStream(new FileOutputStream("my_test.bin"));
+                DataInputStream inputStream = new DataInputStream(new FileInputStream("my_test.bin"))
+                ){
+            outputStream.writeBoolean(false);
+            outputStream.writeShort(2);
+            outputStream.writeDouble(2.453453);
+            outputStream.writeLong(1_000242342L);
+            outputStream.writeInt(1324);
+
+
+
+            System.out.println(inputStream.readBoolean());
+            System.out.println(inputStream.readShort());
+            System.out.println(inputStream.readLong());
+            System.out.println(inputStream.readInt());
+            System.out.println(inputStream.read());
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
