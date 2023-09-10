30925633923cf22564458d450af274bda51990bf
maksud9909 <rmaksud822@gmail.com>
2023-09-10 20:13:02 +0500

    I write some evening code

--- src/Trash/BinaryFiles.java
+++ src/Trash/BinaryFiles.java
@@ -1,8 +1,22 @@
 package Trash;
 
-public class BinaryFiles {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class BinaryFiles {
+    public static void main(String[] args) throws IOException {
+
+       try (
+               DataOutputStream outputStream = new DataOutputStream(new FileOutputStream("data.bin"));
+               DataInputStream inputStream = new DataInputStream(new FileInputStream("data.bin"));
+               ){
+           outputStream.writeInt(234);
+           outputStream.write(new byte[] {23,3,3,3,2,23,6});
+           outputStream.writeBoolean(true);
+
+           System.out.println(inputStream.read());
+           System.out.println(inputStream.readInt());
+           System.out.println(inputStream.readBoolean());
+       }
     }
 
 
