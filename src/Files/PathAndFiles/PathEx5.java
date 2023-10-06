30ee0aaf632f189b504b3d0095e51b6e4bac1add
maksud9909 <rmaksud822@gmail.com>
2023-10-06 14:43:22 +0500

    WalkFireTree

--- src/Files/PathAndFiles/PathEx5.java
+++ src/Files/PathAndFiles/PathEx5.java
@@ -1,4 +1,20 @@
 package Files.PathAndFiles;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class PathEx5 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("MaksudTest.txt");
+//        Files.createFile(path);
+//        String str = new String("My name is Maksud, and i promise that i will do sport at least 3 times a week");
+//        Files.write(path,str.getBytes());
+
+        List<String> list = Files.readAllLines(path);
+        list.forEach(s -> System.out.println(s));
+        System.out.println(list);
+    }
 }
---
+++ src/Files/WalkFileTree/WalkFileTree.java
@@ -0,0 +1,40 @@
+package Files.WalkFileTree;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class WalkFileTree {
+    public static void main(String[] args) throws IOException {
+        Path directoryFile = Paths.get("/Users/maksudrustamov/Desktop/ForFiles");
+        Files.walkFileTree(directoryFile,new MyFileVisitor());
+    }
+}
+
+
+class MyFileVisitor implements FileVisitor<Path>{
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        System.out.println("Enter to directory " + dir);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println("File name " + file.getFileName());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        System.out.println("Error while visiting file: " + file.getFileName());
+        return FileVisitResult.TERMINATE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        System.out.println("Exit from file dir " + dir);
+        return FileVisitResult.CONTINUE;
+    }
+}
