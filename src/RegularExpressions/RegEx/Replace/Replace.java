b43b5c9002b2af692c6b0ddd58559c14c7d83521
maksud9909 <rmaksud822@gmail.com>
2023-10-16 20:21:20 +0500

    RegEx replace

--- src/RegularExpressions/RegEx/Replace/Replace.java
+++ src/RegularExpressions/RegEx/Replace/Replace.java
@@ -2,6 +2,10 @@ package RegularExpressions.RegEx.Replace;
 
 public class Replace {
     public static void main(String[] args) {
+        String info = "My    name    is   Maksud   ";
+        System.out.println(info);
 
+        info = info.replaceAll(" {2,}", " ");
+        System.out.println(info);
     }
 }
---
+++ src/RegularExpressions/RegEx/Replace/Replace2.java
@@ -0,0 +1,24 @@
+package RegularExpressions.RegEx.Replace;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Replace2 {
+    public static void main(String[] args) {
+        String str = "    12345678901234567890123;\n" +
+                "    98765432109876543210987;\n" +
+                "    55555555555555555555555";
+
+        Pattern pattern = Pattern.compile("(\\d{4})(\\d{4})(\\d{4})(\\d{4})(\\d{2})(\\d{2})(\\d{3})");
+        Matcher matcher = pattern.matcher(str);
+
+//        String newString = matcher.replaceAll("$5/$6 $1 $2 $3 $4 CVV $7");
+//        System.out.println(newString);
+
+        while (matcher.find()){
+            System.out.println(matcher.group(1));
+        }
+
+    }
+}
