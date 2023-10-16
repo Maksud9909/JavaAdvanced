6cecac21018c8758fdc94da97c122ecb2da1c398
maksud9909 <rmaksud822@gmail.com>
2023-10-16 15:31:16 +0500

    RegEx

---
+++ src/RegularExpressions/RegEx/Ip/RegEx3.java
@@ -0,0 +1,19 @@
+package RegularExpressions.RegEx.Ip;
+
+import java.util.regex.Pattern;
+
+public class RegEx3 {
+    void checkIp(String ip){
+        String regex = "((25[0-5]|2[0-4]\\d|[01]?\\d?\\d)(\\.)){3}" +
+                "(25[0-5]|2[0-4]\\d |[01]?\\d?\\d)";
+        System.out.println(ip + " is OK? "+ Pattern.matches(regex,ip));
+
+    }
+    public static void main(String[] args) {
+        String ip1 = new String("192.168.1.100");
+        String ip2 = new String("192.16.193.98");
+        RegEx3 regEx3 = new RegEx3();
+        regEx3.checkIp(ip1);
+        regEx3.checkIp(ip2);
+    }
+}
---
+++ src/RegularExpressions/RegEx/Replace/Replace.java
@@ -0,0 +1,7 @@
+package RegularExpressions.RegEx.Replace;
+
+public class Replace {
+    public static void main(String[] args) {
+
+    }
+}
